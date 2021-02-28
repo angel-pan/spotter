68edebba2aca1740c829a6b0eb1e0e636611ed2d
ejyang7 <ejyang7@gmail.com>
2021-02-28 13:39:14 -0800

    Folder setup

---
+++ App/Screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { material } from 'react-native-typography';
+import { Metrics } from '../Themes';
+import { Entypo } from '@expo/vector-icons';
+import Feed from '../Components/Feed'
+
+export default function HomeScreen({ navigation }) {
+
+  return (
+    null
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ App/Themes/Images.js
@@ -0,0 +1,7 @@
+// leave off @2x/@3x
+const images = {
+    placeholder: require('../Images/placeholder.jpg'),
+  }
+  
+  export default images
+  
